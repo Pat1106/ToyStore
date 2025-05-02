4948dd186daaebe4db077abc822e40dfb124b6a1
Pat1106 <patrice.billy@gmail.com>
2025-05-02 15:20:42 +0200

    Addtocart_changes

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -5,7 +5,7 @@ const renderBadge = () => {
   badge.innerText = cartLS.list().reduce((prev, curr) => prev + curr.quantity, 0)
 }
 
-const listenToAdd = (buttons) => {
+/*const listenToAdd = (buttons) => {
   buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
       const { id, name, price, location } = event.currentTarget.dataset
@@ -21,7 +21,25 @@ const listenToAdd = (buttons) => {
       })
     })
   })
+}*/
+
+const listenToAdd = (buttons) => {
+buttons.forEach((button) => {
+button.addEventListener('click', (event) => {
+const { id, name, price } = event.currentTarget.dataset
+if (cartLS.exists(id)) {
+cartLS.quantity(id, 1)
+} else {
+cartLS.add({ id, name, price })
 }
+dataLayer.push({
+event: 'add_to_cart',
+product_id: id,
+product_name: name,
+product_price: Number(price)
+})
+})
+})
 
 const cartItemsListeners = () => {
   const addToCartButtons = document.querySelectorAll('.cart .add-to-cart')
